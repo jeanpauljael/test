710594a9180d58597a275246833268628fe36fdf
jpjael <jeanpauljael@gmail.com>
2021-10-04 13:29:46 +0800

    second commit

--- js/app.js
+++ js/app.js
@@ -1,11 +1,80 @@
+// navbar ===========================================
+
 const navSlide = () => {
     const burger = document.querySelector(".burger");
     const nav = document.querySelector(".nav-links");
-
+    const navLinks = document.querySelectorAll(".nav-links li");
+    
     burger.addEventListener("click", () =>{
+        // toggle nav
         nav.classList.toggle("nav-active");
-        burger.classList.toggle("toggl")
+
+        // animate links
+        navLinks.forEach((link, index) => {
+        
+            if(link.style.animation) {
+                link.style.animation = '';
+            } else {
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.5}s`;
+            }
+        });
+
+        // burger animation
+        burger.classList.toggle("toggle");
+
     });
+    
+    
 };
 
 navSlide();
+
+// gallery ============================================
+// init
+
+function assetsTemplate(heading, description, path) {
+    return `
+      <div class="asset">
+        <img src="./img/${path}" alt="" />
+        <div class="overlay">
+          <div>
+            <h4>${heading}</h4>
+            <p>${description}</p>
+          </div>
+        </div>
+      </div>
+    
+          `;
+  }
+  
+  // get assets
+  function getAssets() {
+    const assetElement = document.querySelector(".galleryWrapper");
+  
+    for (var asset in this) {
+      assetElement.innerHTML += assetsTemplate(
+        this[asset].heading,
+        this[asset].description,
+        this[asset].path
+      );
+    }
+  }
+  
+  // call function to pass value
+  getAssets.call({
+    man: {
+      heading: "Man Image",
+      description: "Image of a man.",
+      path: "man.jpg",
+    },
+    wizard: {
+      heading: "Wizard Image",
+      description: "Image of a wizard.",
+      path: "wizard.jpg",
+    },
+    beast: {
+      heading: "Beast Image",
+      description: "Image of a beast.",
+      path: "beast.jpg",
+    },
+  });
