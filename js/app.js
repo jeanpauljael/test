2a1e6e4d2bd328ec3500318c26f8c8391460eb2c
jpjael <jeanpauljael@gmail.com>
2021-10-04 18:20:37 +0800

    third commit

--- js/app.js
+++ js/app.js
@@ -27,6 +27,16 @@ const navSlide = () => {
     
 };
 
+// navbar add active class ============================
+const list = document.querySelectorAll('.list');
+function activeLink(){
+  list.forEach((item) => 
+  item.classList.remove('active'));
+  this.classList.add('active');
+}
+list.forEach((item) => 
+item.addEventListener('click', activeLink));
+
 navSlide();
 
 // gallery ============================================
